e027e97391cd5afbfa68bfeff3561bf3a6bef084
alvarezaaronai <alvarezaaron80@gmail.com>
2018-11-08 17:56:41 -0800

    added Utils Parser

--- app/src/main/java/com/alvarezaaronai/newsapp/JsonUtils.java
+++ app/src/main/java/com/alvarezaaronai/newsapp/JsonUtils.java
@@ -1,13 +1,39 @@
 package com.alvarezaaronai.newsapp;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class JsonUtils {
 
     public static ArrayList<NewsItem> parseNews(String JSONString){
+        ArrayList<NewsItem> newsItemsTemp = new ArrayList<>();
 
+        try {
+            JSONObject news = new JSONObject(JSONString);
+            JSONObject articles = news.getJSONObject("articles");
+
+            int articlesSize = articles.length();
+            for(int art = 0; art > articlesSize; art++ ) {
+                //Strings for every Key
+                String titleTemp = articles.getString("title");
+                String authorTemp = articles.getString("author");
+                String descriptionTemp = articles.getString("description");
+                String urlTemp = articles.getString("url");
+                String urlImageTemp = articles.getString("imageToUrl");
+                String dateImageTemp = articles.getString("publishedAt");
+                //Creating a NewsTemp
+                NewsItem tempNewsItem = new NewsItem(titleTemp, authorTemp, descriptionTemp, urlTemp, urlImageTemp, dateImageTemp);
+                //Adding to the NewsItemTemp
+                newsItemsTemp.add(tempNewsItem);
+            }
+            //----------------------
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         //Return and ArrayList of NewsItems
-        return null;
+        return newsItemsTemp;
     }
 }
 
--- app/src/main/java/com/alvarezaaronai/newsapp/NewsItem.java
+++ app/src/main/java/com/alvarezaaronai/newsapp/NewsItem.java
@@ -17,7 +17,77 @@ public class NewsItem {
     //Json Val is "publishedAt"
     private String date;
 
-    public NewsItem (JSONObject jsonObjectIn){
 
+    public NewsItem (){
+        title = "UBG goes free-to-play on Xbox to combat waning interest";
+        author = "Rachel Kaser";
+        description = "Microsoft today revealed PlayerUnknown's Battlegrounds would be free-to-play this weekend, the first time the game has ever been free on that platform. It's a pleasant surprise for ...";
+        url = "https://thenextweb.com/gaming/2018/11/08/pubg-free-weekend-xbox-player-interest/";
+        imageUrl = "https://img-cdn.tnwcdn.com/image/tnw?filter_last=1&fit=1280%2C640&url=https%3A%2F%2Fcdn0.tnwcdn.com%2Fwp-content%2Fblogs.dir%2F1%2Ffiles%2F2017%2F12%2FPUBG.jpg&signature=78bb6051436b903034b703931b891605";
+        date = "2018-11-08T09:34:58Z";
+    }
+
+    public NewsItem (String titleIn, String authorIn, String descriptionIn, String urlIn, String imageUrlIn, String dateIn){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "NewsItem{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", description='" + description + '\'' +
+                ", url='" + url + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", date='" + date + '\'' +
+                '}';
     }
 }
